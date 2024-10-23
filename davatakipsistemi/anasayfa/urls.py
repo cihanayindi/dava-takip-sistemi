5d380dc25d15f94b063f76a694c81eeafb233c97
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-10-24 01:51:42 +0300

    İskelet oluşturuldu
    Geçiçi bir iskelet oluşturdum geri silinebilir veya üzerinden devam edilebilir.

---
+++ davatakipsistemi/anasayfa/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+# http://127.0.0.1:8000/ => anasayfa
+# http://127.0.0.1:8000/index => anasayfa
+# http://127.0.0.1:8000/muvekkildetay/1 => 1. müvekkil 
+
+urlpatterns = {
+    path("", views.index),
+    path("index", views.index),
+    path("muvekkildetay/<int:id>", views.muvekkildetay)
+}
---
+++ davatakipsistemi/anasayfa/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "anasayfa/index.html")
+
+def muvekkildetay(request, id):
+    return render(request, "anasayfa/muvekkildetay.html", {
+        "id" : id
+    })
