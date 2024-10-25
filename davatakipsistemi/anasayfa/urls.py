c5c3ec60610be2b84b321c3d011c78649d1e7b02
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-10-25 18:47:15 +0300

    Update urls.py

--- davatakipsistemi/anasayfa/urls.py
+++ davatakipsistemi/anasayfa/urls.py
@@ -4,8 +4,8 @@ from . import views
 # http://127.0.0.1:8000/index => anasayfa
 # http://127.0.0.1:8000/muvekkildetay/1 => 1. müvekkil 
 
-urlpatterns = {
+urlpatterns = [
     path("", views.index),
     path("index", views.index),
     path("muvekkildetay/<int:id>", views.muvekkildetay)
-}
+]
