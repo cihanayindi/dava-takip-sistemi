d99f79761c499c57182b4e32c31c5b74df7b9fae
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-10-29 01:13:31 +0300

    Giriş yapma sistemi geliştirildi
    Veritabanı işlemleri sonrası tamamlanmaya çok yakın.

--- davatakipsistemi/authentication/views.py
+++ davatakipsistemi/authentication/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from davatakipsistemi.backends.user_authenticate import custom_authenticate
 
 def login_request(request):
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
 
-        print(username,password)
+        if custom_authenticate(username, password):
+            pass # burada kullanıcı o an hangi sayfadan geldiyse 
+                # oraya ya da anasayfaya yönlendirilecek
+        else:
+            return render(request, "authentication/login.html", {
+                "error" : "Kullanıcı adı ya da parola yanlış!"})
+
     return render(request, 'authentication/login.html')
 
 def logout_request(request):
---
+++ davatakipsistemi/davatakipsistemi/backends/user_authenticate.py
@@ -0,0 +1,10 @@
+def custom_authenticate(username, password):
+    # bu fonksiyona parametre olarak gelen bilgiler
+    # veritabanına sorgu atılacak ve bu eğer bilgiler eşleşiyorsa
+    # bu fonksiyon true ya da false değeri dönecek dönen değer 
+    # authentication/view.py'de işlenerek kullanıcının sisteme giriş yapıp yapmamasına karar verecek
+
+    # veritabanı işini beceremediğim için bu fonksiyonu böylece bırakıyorum. 
+    
+    pass
+
