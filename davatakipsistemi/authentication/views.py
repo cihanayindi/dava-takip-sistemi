9f2f74d43858da1df079a2e7198fe491c1ad19ee
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-10-29 15:10:06 +0300

    ++++

--- davatakipsistemi/authentication/views.py
+++ davatakipsistemi/authentication/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
 from davatakipsistemi.backends.user_authenticate import custom_authenticate
 
 def login_request(request):
+    # GET isteğinde `next` parametresi varsa oturuma kaydedilir
+    if request.method == 'GET':
+        next_url = request.GET.get('next', '/')
+        request.session['next_url'] = next_url  # Oturumda saklanır
+        print(request.session['next_url'])
+
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
 
         if custom_authenticate(username, password):
-            pass # burada kullanıcı o an hangi sayfadan geldiyse 
-                # oraya ya da anasayfaya yönlendirilecek
+            # Oturumdan `next_url` değeri alınır
+            next_url = request.session.get('next_url', '/')
+            print(next_url)
+            return redirect(next_url)  # Başarılı giriş sonrası `next_url`e yönlendir
         else:
+            # Hatalı girişte `next_url` değeri tekrar sayfaya gönderilir
             return render(request, "authentication/login.html", {
-                "error" : "Kullanıcı adı ya da parola yanlış!"})
+                "error": "Kullanıcı adı ya da parola yanlış!",
+                "next": request.session.get('next_url', '/')
+            })
 
-    return render(request, 'authentication/login.html')
+    return render(request, 'authentication/login.html', {"next": request.session.get('next_url', '/')})
 
 def logout_request(request):
     return render(request, 'authentication/login.html')
