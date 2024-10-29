eff1de1a08b6c1bfb2b2b052217b8355a0b0b636
Cihan Ayindi <cihan_ayindi@hotmail.com>
2024-10-30 02:34:34 +0300

    anasayfaya dava bilgileri tablosu eklendi

--- davatakipsistemi/anasayfa/views.py
+++ davatakipsistemi/anasayfa/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
-
+from Client.models import Case
 # Create your views here.
 
 def index(request):
-    return render(request, "anasayfa/index.html")
+    cases = Case.objects.all()  # Veritabanından tüm dava kayıtlarını çekiyoruz
+    for case in cases:
+        print(case.case_number)
+    return render(request, 'anasayfa/index.html', {'cases': cases})
 
 def muvekkildetay(request, id):
     return render(request, "anasayfa/muvekkildetay.html", {
