dab8dda28d4558def4b629f67ae352211e3ebd1c
sübhan akbenli <suakbenli@gmail.com>
2024-11-01 17:05:06 +0300

    +++

--- davatakipsistemi/fileIO/views.py
+++ davatakipsistemi/fileIO/views.py
@@ -1,12 +1,14 @@
 from .xlsxReader import read_excel_file
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from Client.models import DailyFile, UploadedFile
+from django.contrib import messages
 
 @login_required
 def upload_page(request):
     return render(request, 'fileIO/upload_daily.html')  # upload.html şablonunu yükler
+
 def upload_file(request):
     if request.method == 'POST' and request.FILES['file']:
         excel_file = request.FILES['file']
@@ -24,7 +26,9 @@ def upload_file(request):
         rows = df.values.tolist()
 
         # JSON yanıt döndür
-        return JsonResponse({"headers": headers, "rows": rows})
+        messages.success(request, "Dosya başarıyla yüklendi.")
+        return redirect('upload_page')
+        # return JsonResponse({"headers": headers, "rows": rows})
 
     return JsonResponse({"error": "Dosya yüklenemedi."}, status=400)
 
