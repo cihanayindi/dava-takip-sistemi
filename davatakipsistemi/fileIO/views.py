553ec0b9d23399c7206564feae6fc697aa080604
sübhan akbenli <suakbenli@gmail.com>
2024-11-05 00:30:21 +0300

    daily_file içine toplu excel yüklemesi yapıldı
    +++excel içeriğini görüntüleme kaldırıldı.
    +++ içerik okuma işlemlerinden sonra direkt iş listesi sayfasına yönlendirilecek

--- davatakipsistemi/fileIO/views.py
+++ davatakipsistemi/fileIO/views.py
@@ -1,33 +1,25 @@
-from .xlsxReader import read_excel_file
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from Client.models import DailyFile, UploadedFile
+from Client.models import DailyFile
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required
 def upload_page(request):
-    return render(request, 'fileIO/upload_daily.html')  # upload.html şablonunu yükler
+    return render(request, 'fileIO/upload_daily.html')  # upload_daily.html şablonunu yükler
 
+@csrf_exempt
+@login_required
 def upload_file(request):
-    if request.method == 'POST' and request.FILES['file']:
-        excel_file = request.FILES['file']
-        print("ilk ")
-        # Dosyayı modele kaydet
-        uploaded_file = DailyFile(file=excel_file)
-        uploaded_file.save()
-
-        # Excel dosyasını oku
+    if request.method == 'POST':
+        files = request.FILES.getlist('files')
+        saved_files = []
         
-        df = read_excel_file(excel_file)
+        for excel_file in files:
+            uploaded_file = DailyFile(file=excel_file)
+            uploaded_file.save()
+            saved_files.append(uploaded_file.file.name)
 
-        # Sütun adları ve satırları JSON'a dönüştür
-        headers = list(df.columns)
-        rows = df.values.tolist()
-
-        # JSON yanıt döndür
-        return JsonResponse({"headers": headers, "rows": rows})
+        return JsonResponse({"message": "Dosyalar başarıyla yüklendi.", "files": saved_files})
 
     return JsonResponse({"error": "Dosya yüklenemedi."}, status=400)
-
-
-
