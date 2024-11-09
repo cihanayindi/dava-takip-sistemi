cb2202fbe2bc504ec113d5cf90c5c24585544008
sübhan akbenli <suakbenli@gmail.com>
2024-11-09 20:39:50 +0300

    Dosya Yükleme işlemi bağlantıları
    Yeni modeller ekledim bu modelleriden biri işlem tülreri ve bildirim önceliklerini biri de davaların güncellemelerini tutmamızı sağlayacak
    3 tip dsya türü tanımladım bunlardan tebligat ve duruşma şuan çalışır durumda safahat kısmını henüz tam kontrol edemedim ve hata kontrolleri için genel bir bakış ve kontroller yaptım
    
    artık dosya yüklendiği anda dava içeriği güncelleniyor ve bildirim oluşturuluyor eğer dava yoksa dava otomatik ouşturulup  müvekkil atanması için bildirim çıkıyor
    
    şimdilik bu bildirim atanması için olan bildirim direkt olarak  davaya yönelndiriyor buradan da müvekkil ataması yaparız diye düşündüm gerekirse bağlanddığı linki de değiştirebiliriz
    
    Zaten işlemleri yaparken sitede gördüğüm eksikleri de yapılacaklara ekledim buradan okuyup fikir paylaşımı yaparız

--- davatakipsistemi/Client/models.py
+++ davatakipsistemi/Client/models.py
@@ -5,44 +5,23 @@ import os
 
 
 class Client(models.Model):
-    """
-    Client model to store information about legal clients.
 
-    Attributes:
-        id (AutoField): Unique identifier for the client, automatically incremented.
-        tc (str): Turkish Identification Number, unique for each client.
-        first_name (str): First name of the client.
-        last_name (str): Last name of the client.
-        address (str): Address of the client.
-        phone (str): Phone number of the client.
-        email (str): Email address of the client.
-        agreement_amount (Decimal): Amount agreed upon for the new case.
-        amount_received (Decimal): Amount received from the client.
-        remaining_balance (Decimal): Remaining balance owed by the client.
-        file_expenses (Decimal): Expenses related to the client's case files.
-        last_sms_date (DateField): Date of the last SMS sent to the client.
-        files (str): Files associated with the client.
-        created_at (DateTimeField): Timestamp when the client record was created.
-        updated_at (DateTimeField): Timestamp when the client record was last updated.
-        notes (str): Additional notes about the client.
-    """
-    
     id = models.AutoField(primary_key=True)  # Otomatik artan ve unique id
     tc = models.CharField(max_length=11, unique=True)  # T.C. Kimlik No
     name = models.CharField(max_length=50)  # Ad
     surname = models.CharField(max_length=50)  # Soyad
     address = models.TextField()  # Adres
     phone = models.CharField(max_length=15)  # Telefon
-    email = models.EmailField()  # Mail
+    email = models.EmailField(blank=True , null=True )  # Mail
     agreement_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Yeni davanın anlaşma bedeli
     amount_received = models.DecimalField(max_digits=10, decimal_places=2)  # Alınan para
     remaining_balance = models.DecimalField(max_digits=10, decimal_places=2)  # Kalan bakiye
     file_expenses = models.DecimalField(max_digits=10, decimal_places=2)  # Dosya masrafları
     last_sms_date = models.DateField(null=True, blank=True)  # En son SMS atma tarihi
     files = models.TextField()  # Müvekkilin dosyaları
-
-    # Ek sütunlar
+    
     created_at = models.DateTimeField(auto_now_add=True)  # Oluşturulma tarihi
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # User tracking
     updated_at = models.DateTimeField(auto_now=True)  # Güncellenme tarihi
     notes = models.TextField(blank=True, null=True)  # Notlar
 
@@ -52,34 +31,15 @@ class Client(models.Model):
 
 
 class Case(models.Model):
-    """
-    Model to store information about legal cases associated with clients.
 
-    Attributes:
-        id (AutoField): Unique identifier for the case, automatically incremented.
-        client (ForeignKey): The client associated with this case.
-        case_number (str): Unique case number.
-        case_type (str): Type of the case (e.g., criminal, civil, labor).
-        status (str): Current status of the case (e.g., ongoing, resolved, closed).
-        start_date (DateField): Date when the case started.
-        end_date (DateField): Date when the case ended (if resolved).
-        hearing_date (DateField): Date of the court hearing.
-        court (str): The court where the case is being heard.
-        description (TextField): A brief description of the case.
-        created_at (DateTimeField): Timestamp when the case record was created.
-        updated_at (DateTimeField): Timestamp when the case record was last updated.
-    """
-    
     id = models.AutoField(primary_key=True)  # Otomatik artan ve unique id
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)  # Müvekkil ile ilişki
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True,null=True )  # Müvekkil ile ilişki
     case_number = models.CharField(max_length=50, unique=True)  # Dava numarası
     case_type = models.CharField(max_length=30)                 # Dava türü
     status = models.CharField(max_length=20)                    # Dava durumu
-    # start_date = models.DateField()                             # Başlangıç tarihi
-    # end_date = models.DateField(null=True, blank=True)          # Bitiş tarihi
-    # hearing_date = models.DateField(null=True, blank=True)      # Duruşma tarihi
     court = models.CharField(max_length=100)                    # Mahkeme
     description = models.TextField(blank=True, null=True)       # Dava açıklaması
+    
     created_at = models.DateTimeField(auto_now_add=True)        # Oluşturulma tarihi
     updated_at = models.DateTimeField(auto_now=True)            # Güncellenme tarihi
     updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # User tracking
@@ -88,7 +48,13 @@ class Case(models.Model):
     def __str__(self):
         return f"{self.case_number} - {self.client}"
     
-
+class CaseProgress(models.Model):
+    case = models.ForeignKey(Case, related_name='progress_steps', on_delete=models.CASCADE)  # Davaya bağlı adımlar
+    description = models.TextField(blank=True, null=True)  # Açıklama
+    progress_date = models.DateTimeField()  # İlerleme tarihi
+    created_at = models.DateTimeField(auto_now_add=True)  # Oluşturulma tarihi
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # User tracking
+    
 class ProcessTypes(models.Model):
     id = models.AutoField(primary_key=True)  # Otomatik artan ve unique id
     file_type = models.CharField(max_length=50)
@@ -134,27 +100,16 @@ class UploadedFile(models.Model):
     
 
 class Notification(models.Model):
-    """
-    Model to store notifications related to legal cases or clients.
-
-    Attributes:
-        id (AutoField): Unique identifier for the notification.
-        text (TextField): The notification text.
-        priority (IntegerField): Numeric priority level (1 for Low, 2 for Medium, 3 for High).
-        link (URLField): Associated link for the notification.
-        read (BooleanField): Indicates whether the notification has been read.
-        created_at (DateTimeField): Timestamp when the notification was created.
-        last_action_date (DateTimeField): Timestamp of the last action taken on the notification.
-    """
 
     id = models.AutoField(primary_key=True)  # Unique identifier
     text = models.TextField()  # Notification text
     priority = models.IntegerField()  # Priority level as a numeric value (1, 2, 3)
     link = models.URLField()  # Link associated with the notification
     read = models.BooleanField(default=False)  # Read status
+    deadline_date = models.DateField(blank=True, null=True)  # Deadline date
     created_at = models.DateTimeField(auto_now_add=True)  # Creation timestamp
     last_action_date = models.DateTimeField(auto_now=True)  # Last action timestamp
-
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # User tracking
     class Meta:
         db_table = 'notification'
         ordering = ['-priority', 'read', '-created_at']  # Ordering by priority, read status, and created date
--- davatakipsistemi/fileIO/views.py
+++ davatakipsistemi/fileIO/views.py
@@ -5,8 +5,8 @@ from Client.models import DailyFile
 from django.views.decorators.csrf import csrf_exempt
 from .xlsxReader import read_excel_file
 import pandas as pd
-from Client.models import Case,ProcessTypes
-
+from Client.models import Case,ProcessTypes,CaseProgress,Notification
+from datetime import datetime, timedelta
 
 def add_process_type():
     df =read_excel_file("C:/Users/suakb/supi/bionluk/dava-takip-sistemi/excelIcerikleri/işlem türleri avukat1.xlsx")
@@ -23,57 +23,232 @@ def upload_page(request):
     # add_process_type()
     return render(request, 'fileIO/upload_daily.html')  # upload_daily.html şablonunu yükler
 
+
+
 @csrf_exempt
 @login_required
 def upload_file(request):
+    """Dosyaları yükler ve içeriklerine göre işleme alır."""
     if request.method == 'POST':
         files = request.FILES.getlist('files')
-        file_types = request.POST.getlist('file_types')  # Dosya türlerini alıyoruz
+        file_types = request.POST.getlist('file_types')
         saved_files = []
 
         for index, excel_file in enumerate(files):
-            file_type = file_types[index]  # Dosya tipini al
-            yapilan_kayit = 0
+            file_type = file_types[index]
             df = read_excel_file(excel_file)
-            if file_type == 'safahat':
-                df =df.drop('No',axis=1)
-                basliklar = df.columns.tolist()
-                liste = df.values.tolist()
-                for row in liste:
-                    print(row)
-                    if  "Dosya Türü" in basliklar:
-                        islem_yapan_birim = row[0]
-                        dosya_no = row[1]
-                        dosya_turu = row[2]
-                        islem_turu = row[3]
-                        karar_tarihi = row[4]
-                        aciklama = row[5]
-                        
-                    else:
-                        islem_yapan_birim = row[0]
-                        dosya_no = row[1]
-                        karar_tarihi = row[2]
-                        islem_turu = row[3]
-                        aciklama = row[4]
-                    print(islem_turu)                      
-                    current_process = ProcessTypes.objects.filter(process_type=islem_turu)      
-                    if current_process:
-                        current_process = current_process[0]
-                        print(current_process, current_process.deadline)              
-                    # related_case = Case.objects.get(court = islem_yapan_birim,case_number=dosya_no)
-
-
-            elif file_type == 'tebligat':
-                pass
-            elif file_type == 'duruşma':
-                pass
-            print(yapilan_kayit)
-            print(excel_file,file_type)
             
-            # uploaded_file = DailyFile(file=excel_file, file_type=file_type)
-            # uploaded_file.save()
-            # saved_files.append(uploaded_file.file.name)
+            if file_type == 'safahat':
+                process_safahat_file(df)
+            elif file_type == 'tebligat':
+                process_tebligat_file(df)
+            elif file_type == 'duruşma':
+                process_durusma_file(df)
 
+            saved_files.append(excel_file.name)
+        
         return JsonResponse({"message": "Dosyalar başarıyla yüklendi.", "files": saved_files})
 
     return JsonResponse({"error": "Dosya yüklenemedi."}, status=400)
+
+
+def process_safahat_file(df):
+    """Safahat dosyasını işler ve ilgili case progress'leri oluşturur."""
+    df = df.drop('No', axis=1)
+    basliklar = df.columns.tolist()
+    rows = df.values.tolist()
+
+    for row in rows:
+        islem_yapan_birim, dosya_no, karar_tarihi, islem_turu, aciklama = extract_safahat_row_data(row, basliklar)
+        
+        current_process = ProcessTypes.objects.filter(process_type=islem_turu).first()
+        if not current_process:
+            log_missing_process_type(islem_turu)
+            continue
+        
+        date_obj = datetime.strptime(karar_tarihi, "%d.%m.%Y %H:%M")
+        deadline = date_obj + timedelta(days=current_process.deadline)
+        progress_date=date_obj.strftime("%Y-%m-%d")
+        progress_text = f"Karar Tarihi : {progress_date}\nİşlem Türü:{islem_turu}\nAçıklama:{aciklama} "
+        
+        # İşlem türü bulunan case ile ilgili işlemleri yap
+        related_case = Case.objects.filter(court=islem_yapan_birim, case_number=dosya_no).first()
+        if related_case:
+            notification_text=f"Dava Güncellendi:{islem_yapan_birim} - {dosya_no}\n{islem_turu}"
+            
+            # Mevcut dava güncelleniyor
+            create_case_progress(case=related_case, progress_date=progress_date, description=progress_text)    
+            create_notification(text=notification_text,
+                priority=current_process.priority,
+                link=f"/case/{new_case.id}",
+                deadline_date=progress_date)
+        
+        else:
+            notification_text=f"Safahat ile Otomatik Yeni Dava Eklendi : {islem_yapan_birim} - {dosya_no}\n{islem_turu}"
+            # Yeni dava oluşturuluyor
+            new_case = create_new_case(islem_yapan_birim, dosya_no)
+            create_case_progress(case=new_case, progress_date=progress_date, description=progress_text)    
+            
+            create_notification(text=notification_text,
+                                priority=3,
+                                link=f"/case/{new_case.id}",
+                                deadline_date=progress_date)
+            
+
+def process_tebligat_file(df):
+    """Tebligat dosyasını işler ve ilgili case progress'leri oluşturur."""
+    df = df.drop(['Boyut'], axis=1, errors='ignore')
+    rows = df.values.tolist()
+    for row in rows:
+        gonderen,konu, durum, teslim_tarihi = extract_tebligat_row_data(row)
+        
+        mahkeme, dosya_no = parse_case_details(konu)
+
+        date_obj = datetime.strptime(teslim_tarihi, "%d.%m.%Y %H:%M")
+        progress_date=date_obj.strftime("%Y-%m-%d")
+        
+        progress_text = f"""Gönderen: {gonderen} Tebligat Durumu : {durum} - Teslim Tarihi : {progress_date}"""
+        related_case = Case.objects.filter(court=mahkeme, case_number=dosya_no).first()
+        if related_case:
+            notification_text = f"Davanıza Tebligat Geldi :{mahkeme}-{dosya_no}"
+            deadline = date_obj + timedelta(days=5)
+            deadline = deadline.strftime("%Y-%m-%d")
+            create_case_progress(case=related_case,
+                                 description=progress_text, 
+                                 progress_date= progress_date)
+            create_notification(text=notification_text, 
+                                priority=1, 
+                                link=f"/case/{related_case.id}", 
+                                deadline_date=deadline)
+        else:
+            notification_text = f"Tebligat ile Yeni Dava Eklendi, müvekkil ekleyiniz : {mahkeme}-{dosya_no}"
+            
+            new_case = create_new_case(mahkeme, dosya_no)
+            create_case_progress(case=new_case,
+                                 description=progress_text,
+                                 progress_date=progress_date)
+            create_notification(text=notification_text,
+                                priority=3,
+                                link=f"/case/{new_case.id}" ,
+                                deadline_date=progress_date)
+
+
+def process_durusma_file(df):
+    """Duruşma dosyasını işler ve ilgili case progress'leri oluşturur."""
+    df = df.drop('İzinli Hakim', axis=1, errors='ignore')
+    rows = df.values.tolist()
+    
+    for row in rows:
+        mahkeme, dosya_no, dosya_turu, durusma_tarihi, taraf_bilgi, islem, sonuc = extract_durusma_row_data(row)
+        progress_text = generate_progress_text_durusma(durusma_tarihi, dosya_turu, islem, sonuc, taraf_bilgi)
+        
+        
+        # progress_date = format_datetime(durusma_tarihi)
+        date_obj = datetime.strptime(durusma_tarihi, "%d.%m.%Y %H:%M:%S")
+        progress_date = date_obj.strftime("%Y-%m-%d %H:%M:%S")
+        related_case = Case.objects.filter(court=mahkeme, case_number=dosya_no).first()
+        
+        if related_case:
+            notification_text = f"Davanıza Yeni Duruşma Tarihi Eklendi : {mahkeme}-{dosya_no}"
+            deadline = date_obj + timedelta(days=5)
+            deadline = deadline.strftime("%Y-%m-%d")
+            create_case_progress(case=related_case,
+                                 description=progress_text, 
+                                 progress_date= progress_date)
+            create_notification(text=notification_text, 
+                                priority=1, 
+                                link=f"/case/{new_case.id}", 
+                                deadline_date=deadline)
+        
+        else:
+            
+            notification_text = f"Duruşma ile Dava Eklendi, müvekkil ekleyiniz : {mahkeme}-{dosya_no}"
+            new_case = create_new_case(mahkeme, dosya_no)
+            deadline = date_obj + timedelta(days=5)
+            deadline = deadline.strftime("%Y-%m-%d")
+            
+            create_case_progress(case=new_case,
+                                 description=progress_text,
+                                 progress_date=progress_date)
+            create_notification(text=notification_text,
+                                priority=3,
+                                link=f"/case/{new_case.id}" ,
+                                deadline_date=deadline)
+
+
+def update_case_progress(related_case, islem_turu, aciklama, karar_tarihi):
+    """Mevcut dava için ilerleme kaydeder."""
+    
+    
+
+def parse_case_details(konu):
+    """Konudan mahkeme ve dosya numarasını çıkarır."""
+    konu = (konu.split("["))[-1].split("-")
+    mahkeme = konu[0].strip()
+    dosya_no = konu[-1].replace("]","").strip()
+            
+    return mahkeme, dosya_no
+
+def extract_safahat_row_data(row, basliklar):
+    """Safahat dosyasındaki bir satırdan gerekli veriyi çıkartır."""
+    if "Dosya Türü" in basliklar:
+        return row[0], row[1], row[4], row[3], row[5]
+    else:
+        return row[0], row[1], row[2], row[3], row[4] if len(row) > 4 else None
+
+
+def log_missing_process_type(islem_turu):
+    """İşlem türü bulunamadığında log kaydeder."""
+    with open("bulunamayan.txt", "a+", encoding="utf-8") as f:
+        f.write(islem_turu + "\n")
+    print("İşlem türü bulunamadı")
+
+
+def create_new_case(mahkeme, dosya_no):
+    """Yeni dava kaydeder."""
+    new_case = Case(court=mahkeme, case_number=dosya_no)
+    new_case.save()
+    return new_case
+
+
+def create_case_progress(case, progress_date, description=None):
+    """Dava ilerleme kaydeder."""
+    case_progress = CaseProgress(case=case, description=description, progress_date=progress_date)
+    case_progress.save()
+
+
+def create_notification(text,priority =3,link=None, deadline_date=None):
+    """Yeni bir bildirim oluşturur."""
+    notification = Notification(text=text,
+                                priority=priority,
+                                link=link,
+                                deadline_date=deadline_date)
+    notification.save()
+
+
+def generate_progress_text_durusma(durusma_tarihi, dosya_turu, islem, sonuc, taraf_bilgi):
+    """Duruşma için ilerleme metni oluşturur."""
+    return f"Dava Duruşma Güncellemesi Tarih:{durusma_tarihi}\nDosya Türü: {dosya_turu} - İşlem: {islem} - Sonuç: {sonuc}\nTaraf Bilgisi: {taraf_bilgi}"
+
+
+def extract_tebligat_row_data(row):
+    """Tebligat dosyasındaki bir satırdan gerekli veriyi çıkartır."""
+    gonderen = row[0]
+    konu = row[1]
+    durum = row[2]
+    teslim_tarihi = row[3]
+    return gonderen, konu, durum, teslim_tarihi
+
+
+def extract_durusma_row_data(row):
+    """Duruşma dosyasındaki bir satırdan gerekli veriyi çıkartır."""
+    return row[0], row[1], row[2], row[3], row[4], row[5], row[6]
+
+
+
+
+
+def format_datetime(date_str):
+    """Tarihi ve saati doğru formatta dönüştürür."""
+    date_obj = datetime.strptime(date_str, "%d.%m.%Y %H:%M:%S")
+    return date_obj.strftime("%Y-%m-%d %H:%M:%S")
