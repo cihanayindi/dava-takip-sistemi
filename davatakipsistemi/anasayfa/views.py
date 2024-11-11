572ae640ddd81a3509a9af6b4246c6dcffd74ada
sübhan akbenli <suakbenli@gmail.com>
2024-11-12 02:38:48 +0300

    Entegre edilen kısım hata kontrolleri yapılıyor (Google api çağırmasını kapattım sen de yaptıysan çakışmaması için kontrol et)
    ilerleme kaydedilen ve yapılan işlemler
    file io  içinde hata kontrolleri ve safahat için  genel kontroller yapılacak ()
    dosya yükleme ekranı arayüzü düzeltilecek
    dosya sil ikonu ekle
    notification uyarıları ve progress textleri için kontrolller yap hatalı metinler var

--- davatakipsistemi/anasayfa/views.py
+++ davatakipsistemi/anasayfa/views.py
@@ -35,26 +35,26 @@ def index(request):
     creds = None
     events = []
     
-    token_path = os.path.join(os.path.dirname(__file__), "token.json")
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # token_path = os.path.join(os.path.dirname(__file__), "token.json")
+    # if os.path.exists(token_path):
+    #     creds = Credentials.from_authorized_user_file(token_path, SCOPES)
 
 
-    if creds:
-        try:
-            service = build("calendar", "v3", credentials=creds)
-            now = datetime.datetime.utcnow().isoformat() + "Z"  # UTC formatında şu anki zaman
-            events_result = service.events().list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults=10,
-                singleEvents=True,
-                orderBy="startTime",
-            ).execute()
-            events = events_result.get("items", [])
+    # if creds:
+    #     try:
+    #         service = build("calendar", "v3", credentials=creds)
+    #         now = datetime.datetime.utcnow().isoformat() + "Z"  # UTC formatında şu anki zaman
+    #         events_result = service.events().list(
+    #             calendarId="primary",
+    #             timeMin=now,
+    #             maxResults=10,
+    #             singleEvents=True,
+    #             orderBy="startTime",
+    #         ).execute()
+    #         events = events_result.get("items", [])
             
-        except HttpError as error:
-            print(f"Google Calendar API Hatası: {error}")
+    #     except HttpError as error:
+    #         print(f"Google Calendar API Hatası: {error}")
 
     # Hem case verilerini hem de takvim etkinliklerini şablona gönder
     return render(request, "anasayfa/index.html", {"page_obj": page_obj, "events": events})
